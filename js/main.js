ff27ad14f371ed794c675032614b9b90786cf6fa
Mateusz Łubianka <lubiankamateusz@gmail.com>
2018-11-14 15:07:50 +0100

    start project

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const $inputs = document.querySelectorAll('.controls input');
+    
+    function update() {
+        const suffix = this.dataset.sizing || "";
+        document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    }
+    
+    $inputs.forEach($input => $input.addEventListener('change', update));
+    $inputs.forEach($input => $input.addEventListener('mousemove', update));
+});
